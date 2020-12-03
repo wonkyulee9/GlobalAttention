4370b1fc35123eec7fa259db2f8b0fb13b19b5d5
wonkyulee9 <47019482+wonkyulee9@users.noreply.github.com>
2020-12-03 23:31:16 +0900

    Update global_attention.py

--- global_attention.py
+++ global_attention.py
@@ -58,7 +58,114 @@ class GlobalAttention(nn.Module):
         out = (soft_qk * v).sum([5, 6])
         out = out.view(batch, self.out_c, height, width)
         return out
+    
+    
+class GlobalAttentionWithDecay(nn.Module):
+    def __init__(self, in_c, out_c, n_head, dims, k, bias=False):
+        super(GlobalAttentionWithDecay, self).__init__()
 
+        assert out_c % n_head == 0, "out_c must be divisible by n_head"
+        assert (out_c // n_head) % 2 == 0, "channel dimension for each head must be even"
+        self.out_c = out_c
+        self.n_head = n_head
+        self.head_c = out_c // n_head
+        self.rel_c = self.head_c // 2
+
+        self.rel_dim = dims * 2 - 1
+        self.rel_x = nn.Parameter(torch.randn(self.n_head, self.rel_c, self.rel_dim, 1), requires_grad=True)
+        self.rel_y = nn.Parameter(torch.randn(self.n_head, self.rel_c, 1, self.rel_dim), requires_grad=True)
+
+        i, j = np.indices((dims * 2 - 1, dims * 2 - 1)) - dims + 1
+        dec = np.maximum(np.abs(i), np.abs(j))
+        dec = torch.FloatTensor(dec)
+
+        self.register_buffer('dec', dec)
+
+        self.k = nn.Parameter(torch.FloatTensor([k]), requires_grad=True)
+        self.q_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+        self.k_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+        self.v_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        batch, in_c, height, width = x.size()
+        q = self.q_conv(x)
+        v = self.v_conv(x)
+        k = self.k_conv(x)
+
+        q = q.view(batch, self.n_head, self.head_c, height, width, 1, 1)
+        k = k.view(batch, self.n_head, self.head_c, 1, 1, height, width)
+        v = v.view(batch, self.n_head, self.head_c, 1, 1, height, width)
+
+
+        pos_rel = torch.cat((self.rel_x.expand(self.n_head, self.rel_c, self.rel_dim, self.rel_dim),
+                            self.rel_y.expand(self.n_head, self.rel_c, self.rel_dim, self.rel_dim)), dim=1)
+        pos_rel = pos_rel.unfold(2, height, 1).unfold(3, width, 1).flip(4, 5)
+        k = k.expand(batch, self.n_head, self.head_c, height, width, height, width).contiguous()
+        k += pos_rel
+
+        qk = (q * k).sum([2])
+        dec = 1 / ((1. + torch.abs(self.k)) ** self.dec)
+        qk *= dec.unfold(0, height, 1).unfold(1, width, 1).flip(2, 3)
+        soft_qk = F.softmax(qk.flatten(4, 5), -1)
+        soft_qk = soft_qk.view(batch, self.n_head, 1, height, width, height, width)
+
+        out = (soft_qk * v).sum([5, 6])
+        out = out.view(batch, self.out_c, height, width)
+        return out
+    
+    
+class GlobalAttentionWithTanh(nn.Module):
+    def __init__(self, in_c, out_c, n_head, dims, k, bias=False):
+        super(GlobalAttentionWithTanh, self).__init__()
+
+        assert out_c % n_head == 0, "out_c must be divisible by n_head"
+        assert (out_c // n_head) % 2 == 0, "channel dimension for each head must be even"
+        self.out_c = out_c
+        self.n_head = n_head
+        self.head_c = out_c // n_head
+        self.rel_c = self.head_c // 2
+
+        self.rel_dim = dims * 2 - 1
+        self.rel_x = nn.Parameter(torch.randn(self.n_head, self.rel_c, self.rel_dim, 1), requires_grad=True)
+        self.rel_y = nn.Parameter(torch.randn(self.n_head, self.rel_c, 1, self.rel_dim), requires_grad=True)
+
+        i, j = np.indices((dims * 2 - 1, dims * 2 - 1)) - dims + 1
+        dec = np.maximum(np.abs(i), np.abs(j))
+        dec = torch.FloatTensor(dec)
+
+        self.register_buffer('dec', dec)
+
+        self.k = nn.Parameter(torch.FloatTensor([k]), requires_grad=False)
+        self.q_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+        self.k_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+        self.v_conv = nn.Conv2d(in_c, out_c, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        batch, in_c, height, width = x.size()
+        q = self.q_conv(x)
+        v = self.v_conv(x)
+        k = self.k_conv(x)
+
+        q = q.view(batch, self.n_head, self.head_c, height, width, 1, 1)
+        k = k.view(batch, self.n_head, self.head_c, 1, 1, height, width)
+        v = v.view(batch, self.n_head, self.head_c, 1, 1, height, width)
+
+
+        pos_rel = torch.cat((self.rel_x.expand(self.n_head, self.rel_c, self.rel_dim, self.rel_dim),
+                            self.rel_y.expand(self.n_head, self.rel_c, self.rel_dim, self.rel_dim)), dim=1)
+        pos_rel = pos_rel.unfold(2, height, 1).unfold(3, width, 1).flip(4, 5)
+        k = k.expand(batch, self.n_head, self.head_c, height, width, height, width).contiguous()
+        k += pos_rel
+
+        qk = (q * k).sum([2])
+        dec = torch.tanh(-5*(self.dec-(10*self.k)))/2 + 0.5
+        qk *= dec.unfold(0, height, 1).unfold(1, width, 1).flip(2, 3)
+        soft_qk = F.softmax(qk.flatten(4, 5), -1)
+        soft_qk = soft_qk.view(batch, self.n_head, 1, height, width, height, width)
+
+        out = (soft_qk * v).sum([5, 6])
+        out = out.view(batch, self.out_c, height, width)
+        return out    
 
 class GlobalAttentionLayer(nn.Module):
     def __init__(self, in_c, out_c, n_head, dims, ff=None, norm='bn', bias=False):
